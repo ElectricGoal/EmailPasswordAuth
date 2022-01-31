8f03474d17921bca565631fa214a29dc036b8c97
ElectricGoal <minhtu250802@gmail.com>
2022-01-31 15:25:57 +0700

    modified and added dark mode feature

--- src/lib/screens/profile_screen.dart
+++ src/lib/screens/profile_screen.dart
@@ -27,14 +27,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
           icon: const Icon(
             Icons.close,
-            color: Colors.black,
           ),
           onPressed: () {
             Provider.of<ProfileManager>(context, listen: false)
@@ -46,12 +44,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ],
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               buildProfile(),
+              const SizedBox(
+                height: 14,
+              ),
+              buildDarkModeSwitch(context),
             ],
           ),
         ),
@@ -68,40 +71,50 @@ class _ProfileScreenState extends State<ProfileScreen> {
           color: Colors.amber,
         ),
         const SizedBox(
-          height: 16,
+          height: 25,
         ),
-        Text(
-          '${widget.user.firstName} ${widget.user.lastName}',
-          style: const TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
+        ListTile(
+          leading: const Text(
+            'Full name:',
+            style: TextStyle(fontSize: 18),
+          ),
+          trailing: Text(
+            '${widget.user.firstName} ${widget.user.lastName}',
+            style: const TextStyle(
+              fontSize: 18,
+            ),
           ),
         ),
         const SizedBox(
-          height: 16,
+          height: 14,
         ),
-        Text(
-          '${widget.user.email} ',
-          style: const TextStyle(fontSize: 21),
+        ListTile(
+          leading: const Text(
+            'Email:',
+            style: TextStyle(fontSize: 18),
+          ),
+          trailing: Text(
+            '${widget.user.email} ',
+            style: const TextStyle(fontSize: 18),
+          ),
         ),
       ],
     );
   }
 
-  Widget buildLogoutButton(){
+  Widget buildLogoutButton() {
     return Container(
       padding: const EdgeInsets.only(right: 16),
       child: TextButton(
         onPressed: () {
-           logout();
+          logout();
 
-            Provider.of<ProfileManager>(context, listen: false)
-                .tapOnProfile(false);
+          Provider.of<ProfileManager>(context, listen: false)
+              .tapOnProfile(false);
 
-            Provider.of<AppStateManager>(context, listen: false).logout();
+          Provider.of<AppStateManager>(context, listen: false).logout();
 
-            Provider.of<ProfileManager>(context, listen: false)
-                .logout();
+          Provider.of<ProfileManager>(context, listen: false).logout();
         },
         child: const Text(
           'Logout',
@@ -110,6 +123,24 @@ class _ProfileScreenState extends State<ProfileScreen> {
             color: Colors.green,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildDarkModeSwitch(BuildContext context) {
+    return ListTile(
+      leading: const Text(
+        'Dark mode',
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
+      trailing: Switch(
+        activeColor: Colors.greenAccent,
+        value: Provider.of<ProfileManager>(context, listen: false).darkMode,
+        onChanged: (value) {
+          Provider.of<ProfileManager>(context, listen: false).darkMode = value;
+        },
       ),
     );
   }
